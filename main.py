0a7d1be3be8dab7a1536c16400966badad06fe64
vegedra <vegedracp@gmail.com>
2024-04-06 17:17:01 -0300

    menuzinho

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ blt.set("""
     window.title='Still Alive';
     input: filter={keyboard};
     window.size=110x39;
+    window.cellsize=9x15;
 """)
 
 # Função que mostra as artes ASCII
@@ -58,7 +59,7 @@ def sing(text, x, y, tempo):
         # Caso o usuario queira sair
         if blt.has_input():
             key = blt.read()
-            if key in (blt.TK_CLOSE, blt.TK_5, blt.TK_ESCAPE):
+            if key in (blt.TK_CLOSE, blt.TK_ESCAPE):
                 sys.exit()
 
 def main():
@@ -66,17 +67,35 @@ def main():
     blt.refresh()
     blt.color('#FFB000')
     blt.bkcolor('black')
+    art('res/frame.txt', 1, 0)
+    blt.printf(69, 3, "Still Alive in Python")
+    blt.printf(71, 5, "By Pedro Ivo, 2024")
     blt.refresh()
-    play_music('res/still_alive.mp3', volume=0.9)
 
     # Main Loop
-    while True:
-        # TODO: Menuzinho na caixa da direita com algumas configurações e pra dar play
-        blt.clear()
-        art('res/frame.txt', 1, 0)
-        blt.printf(69, 3, "Still Alive in Python")
-        blt.printf(71, 5, "By Pedro Ivo, 2024")
+    while (blt.TK_CLOSE != True):
+      blt.printf(74, 10, "1) Play")
+      blt.printf(74, 11, "2) Settings")
+      blt.printf(74, 12, "3) Exit")
+      blt.refresh()
+        
+      key = blt.read()
+      
+      if key in (blt.TK_CLOSE, blt.TK_3, blt.TK_ESCAPE):
+        sys.exit()
+        
+      elif (key == blt.TK_2):
+        blt.clear_area(70, 10, 20, 5)
+        blt.printf(77, 11, "INDEV")
         blt.refresh()
+        blt.delay(2000)
+        main()
+      
+      elif (key == blt.TK_1):
+        play_music('res/still_alive.mp3', volume=0.9)
+        blt.clear_area(70, 10, 20, 5)
+        #blt.clear()
+        
         #blt.delay(500)
         #sing('Forms FORM-29827281-12:', 2,1, 70)  
         #sing('Test Assessment Report', 2,2, 70)  
@@ -127,27 +146,37 @@ def main():
         
         sing("I'm not even angry.", 2,6, 70)  
         time.sleep(2.7)
-        sing("I'm being so sincere right now.", 2,7, 80)  
+        sing("I'm being so sincere right now.", 2,7, 80) 
+        time.sleep(1.6)        
         sing("Even though you broke my heart.", 2,8, 85)  
         sing("And killed me.", 2,9, 80)  
+        time.sleep(2)  
         sing("And tore me to pieces.", 2,10, 70)  
-        sing("And threw every piece into a fire.", 2,11, 70)  
+        time.sleep(2)  
+        sing("And threw every piece into a fire.", 2,11, 80)  
+        time.sleep(1.6)  
         sing("As they burned it hurt because.", 2,12, 70)  
         sing("I was so happy for you!", 2,13, 70)  
-        sing("Now these points of data", 2,14, 70)  
-        sing("make a beautiful line.", 2,15, 70)  
-        sing("And we're out of beta.", 2,16, 70)  
-        sing("We're releasing on time.", 2,17, 70)  
-        sing("So i'm GLaD. I got burned.", 2,18, 70)  
-        sing("Think of all the things we learned", 2,19, 70)  
-        sing("for the people who are", 2,20, 70)  
-        sing("still alive.", 2,21, 70)  
+        time.sleep(0.5)  
+        sing("Now these points of data", 2,14, 60)  
+        sing("make a beautiful line.", 2,15, 60)  
+        sing("And we're out of beta.", 2,16, 60)  
+        sing("We're releasing on time.", 2,17, 60)  
+        sing("So i'm GLaD. I got burned.", 2,18, 60)  
+        sing("Think of all the things we learned", 2,19, 60)  
+        sing("for the people who are", 2,20, 60)  
+        sing("still alive.", 2,21, 60)  
+        time.sleep(1)  
         
         # Tela 3
-        sing('Forms FORM-55551-6:', 2,1, 70)  
-        sing('Personnel File Addendum Addendum:', 2,2, 70) 
+        blt.clear_area(2, 1, 50, 30)
+        sing('Forms FORM-55551-6:', 2,1, 20)  
+        sing('Personnel File Addendum Addendum:', 2,2, 20) 
         
-        sing("One last thing:", 2,4, 70)  
+        sing("One last thing:", 2,4, 30)  
+        
+        blt.clear()
+        main()
 
 if __name__ == "__main__":
     main()
